2589fbf31e28d1bb5f2c2578e87f0a47417d45fb
Jose E. Nunez <je.nunez@yahoo.ca>
2019-01-07 20:18:46 -0500

    report alerts without self-corrective actions

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ subject to change. The documentation can be inaccurate.
 # Notes:
 
 A self-corrective system is related to Systems Engineering, for, ideally, a
-system in this discipline should have corrective actions, either interior or
-exterior corrective actions. Thus, for example, the more self-corrective a
-system is, then the more autonomous it can be (ie., the former is a necessary
-condition for the latter). Besides, Petri-Nets are a well-known model to
-represent Systems Engineering (see, e.g.,
+system in this discipline should have corrective actions, either
+interior (self) or exterior corrective actions. Thus, for example, the more
+self-corrective a system is, then the more autonomous it can be (ie., the
+former is a necessary condition for the latter). Besides, Petri-Nets are a
+well-known model to represent Systems Engineering (see, e.g.,
 [ISO/IEC 15909-1:2004 Systems and software engineering -- High-level Petri nets -- Part 1: Concepts, definitions and graphical notation](https://www.iso.org/standard/38225.html)
 and
 [ISO/IEC 15909-2:2011 Systems and software engineering -- High-level Petri nets -- Part 2: Transfer format](https://www.iso.org/standard/43538.html).)
@@ -23,13 +23,20 @@ and
 
 This program reads a Petri Net system from a XML file in [PNML format](https://en.wikipedia.org/wiki/Petri_Net_Markup_Language).
 
-Example execution:
+Example execution through Maven:
 
+      $ mvn
+      [...]
       Alerts in the system:
-      Alert: This is my normal state #1->handling_my_alert_1
-      Alert: This is my normal state #1->handling_my_alert_0
-       
+      Alert: 'This is my normal state #1' -> 'handling_my_alert_2'
+      Alert: 'This is my normal state #1' -> 'handling_my_alert_1'
+      Alert: 'This is my normal state #1' -> 'handling_my_alert_0'
+            
       Corrective Actions in the system:
-      Corrective-Action: handling_my_alert_1->This is my corrected state #2
-      Corrective-Action: handling_my_alert_0->This is my corrected state #2
+      Corrective-Action: 'handling_my_alert_1' -> 'This is my corrected state #2'
+      Corrective-Action: 'handling_my_alert_0' -> 'This is my corrected state #2'
+            
+      Alerts without Corrective Actions:
+      Alert without corrective-action: 'handling_my_alert_2'
+
 
--- src/main/java/SelfCorrectiveSystem.java
+++ src/main/java/SelfCorrectiveSystem.java
@@ -1,15 +1,19 @@
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jbpt.petri.Flow;
 import org.jbpt.petri.NetSystem;
 import org.jbpt.petri.Node;
+import org.jbpt.petri.Place;
 import org.jbpt.petri.Transition;
 import org.jbpt.petri.io.PNMLSerializer;
 
 public final class SelfCorrectiveSystem {
 
-  private static String defaultPetriNetPNMLFile = "src/main/resources/sample_petri_net.pnml";
+  private static String defaultPetriNetPNMLFile =
+                        "src/main/resources/sample_petri_net.pnml";
 
   protected NetSystem petriNet = null;
 
@@ -23,13 +27,15 @@ public final class SelfCorrectiveSystem {
     System.out.println("\nAlerts in the system:");
 
     for (Flow arc: petriNet.getFlow()) {
-         Node src = arc.getSource();
-         Node dst = arc.getTarget();
+      Node src = arc.getSource();
+      Node dst = arc.getTarget();
 
-         if (src instanceof Node &&
-             dst instanceof Transition) {
-              System.out.println("Alert: " + arc);
-         }
+      if (src instanceof Place
+             && dst instanceof Transition) {
+        System.out.println(
+             String.format("Alert: '%s' -> '%s'", src, dst)
+        );
+      }
     }
   }
 
@@ -38,28 +44,78 @@ public final class SelfCorrectiveSystem {
     System.out.println("\nCorrective Actions in the system:");
 
     for (Flow arc: petriNet.getFlow()) {
-         Node src = arc.getSource();
-         Node dst = arc.getTarget();
-         if (src instanceof Transition &&
-             dst instanceof Node) {
-              System.out.println("Corrective-Action: " + arc);
-         }
+      Node src = arc.getSource();
+      Node dst = arc.getTarget();
+      if (src instanceof Transition
+             && dst instanceof Place) {
+        System.out.println(
+             String.format("Corrective-Action: '%s' -> '%s'", src, dst)
+        );
+      }
     }
+  }
+
+  protected void dumpAlertsWithoutCorrectiveActions() {
+
+    System.out.println("\nAlerts without Corrective Actions:");
+
+    // The end of an alert (arc in the Petri Net) is a transition in the
+    // Petri Net. A corrective action is an arc starting in a transition
+    // and ending in a place in the Petri Net. So an alert without
+    // corrective action is, in the Petri Net, a transition which doesn't
+    // have an outgoing arc from it to any place. (This method doesn't
+    // not trigger an execution in the Petri Net. Nor it analyzes how fast
+    // the outgoing arc consumes the amount in the transition.)
+
+    Set<Transition> transitionsWithoutOutgoingArcs =
+                                petriNet.getTransitions();
+    Set<Transition> transitionsWithIncomingArcs = new HashSet<Transition>();
+
+    for (Flow arc: petriNet.getFlow()) {
+      Node src = arc.getSource();
+      Node dst = arc.getTarget();
+      if (src instanceof Transition
+          && dst instanceof Place) {
+        // "src" has a corrective action
+        transitionsWithoutOutgoingArcs.remove(src);
+      } else if (src instanceof Place
+                 && dst instanceof Transition) {
+        // "dst" has an incoming alert
+        transitionsWithIncomingArcs.add((Transition) dst);
+      } else {
+        System.err.println(
+             String.format("Unknown extremes of arc: '%s'", arc)
+        );
+      }
+    }
+
+    transitionsWithoutOutgoingArcs.retainAll(transitionsWithIncomingArcs);
+
+    for (Transition transition: transitionsWithoutOutgoingArcs) {
+      System.out.println(
+           String.format("Alert without corrective-action: '%s'",
+                         transition)
+      );
+    }
+
   }
 
   public static void main(final String[] args) throws Exception {
 
     SelfCorrectiveSystem scSystem = new SelfCorrectiveSystem();
 
-    String fnamePNML;
+    String fnamePNML = defaultPetriNetPNMLFile;
     // try to get this PNML from the command-line arguments, if given,
     // otherwise try to use the default demo PNML file.
-    fnamePNML = (args.length >= 1) ? args[0] : defaultPetriNetPNMLFile;
+    if (args.length >= 1) {
+      fnamePNML = args[0];
+    }
 
     scSystem.loadPetriNet(fnamePNML);
     scSystem.dumpPetriNetAlerts();
     scSystem.dumpPetriNetCorrectiveActions();
 
+    scSystem.dumpAlertsWithoutCorrectiveActions();
   }
 
 }
